2e835c9451117fd4e62b5eb8c92f19e9513cf5c9
Shuonan Niu <47533062+BuffaloPulpo@users.noreply.github.com>
2022-09-25 23:59:53 -0500

    added port number set

--- description.py
+++ description.py
@@ -1,14 +1,24 @@
 import glob
 import os
 
+from helpers import get_port_number_from_filepath, get_protocol_type_from_filepath
+
 datasets_path = r"D:\all_rapid7_datasets_csv"
 folder_path = datasets_path
 dirs = os.listdir(folder_path)
 
 all_files = glob.glob(folder_path+"/*")
 # how many possible port number and associated network services?
+set_of_port_numbers = set()
 for file in all_files:
     print(file)
+    port_number = get_port_number_from_filepath(file)
+    print(port_number)
+    set_of_port_numbers.add(port_number)
+    protocol_type = get_protocol_type_from_filepath(file)
+    print(protocol_type)
+print(len(set_of_port_numbers))
+print(set_of_port_numbers)
 # extract port number, add it to set and see what we have
 
 # for each port number, figure out the number of files, count of each and aggregate to avg count
---
+++ helpers.py
@@ -0,0 +1,23 @@
+import re
+
+regex_port_number = r'(\d+)(?!.*\d)'
+regex_date = r'(\d{4}-\d{2}-\d{2})'
+
+
+def get_port_number_from_filepath(file_path):
+    match_port = (re.search(regex_port_number, file_path)).group()
+    return match_port
+
+
+def get_date_from_filepath(file_path):
+    match_date = (re.search(regex_date,file_path)).group()
+    return match_date
+
+
+def get_protocol_type_from_filepath(file_path):
+    hi_protocol = 'TBA'
+    if 'tcp' in file_path:
+        hi_protocol = 'TCP'
+    else:
+        hi_protocol = 'UDP'
+    return hi_protocol
